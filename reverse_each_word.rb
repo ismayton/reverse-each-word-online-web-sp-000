d1f06e3cd032b32f59c7a225048e744cd84b068f
Ian Mayton <ian.mayton@gmail.com>
2020-07-17 19:05:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   reversed_array = []
   array.collect do |word|
-    word.reverse
+    reversed_array << word.reverse
   end
-  array.join(" ")
+  reversed_array.join(" ")
 end
